aca186476c875e58970aae162fff72f57cdf93d1
Alison Tamayo <aatamayo2@espe.edu.ec>
2026-01-18 05:09:07 -0500

    Sync final: Limpieza total y POMs corregidos

--- src/main/java/com/bancario/nucleo/dto/iso/ActorISO.java
+++ src/main/java/com/bancario/nucleo/dto/iso/ActorISO.java
@@ -1,11 +1,14 @@
 package com.bancario.nucleo.dto.iso;
 
 import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonAlias;
 
 @Data
 public class ActorISO {
     private String name;
     private String accountId;
     private String accountType;
+
+    @JsonAlias("bankId")
     private String targetBankId;
 }
